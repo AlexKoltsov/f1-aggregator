281e1eeec18309dd3c08940b1dd8ee0458aee022
Koltsov Alexander <Kotlsov_Alexander@cat.com>
2020-12-19 21:33:38 +0300

    Init version with spring io

---
+++ src/main/java/com/koltsa/f1/aggregator/F1AggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.koltsa.f1.aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1AggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F1AggregatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koltsa/f1/aggregator/F1AggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koltsa.f1.aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1AggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
