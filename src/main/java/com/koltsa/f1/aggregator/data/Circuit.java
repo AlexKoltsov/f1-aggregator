d3278551f7b20922b2ab92dbd1a8181591e721c3
Koltsov Alexander <Kotlsov_Alexander@cat.com>
2020-12-20 15:36:43 +0300

    Circuit changed a little bit

--- src/main/java/com/koltsa/f1/aggregator/data/Circuit.java
+++ src/main/java/com/koltsa/f1/aggregator/data/Circuit.java
@@ -12,30 +12,13 @@ import javax.persistence.Table;
 @Data
 public class Circuit {
     @Id
-    @Column(name = "\"circuitId\"", nullable = false)
     private Integer circuitId;
-
-    @Column(name = "\"circuitRef\"", nullable = false)
     private String circuitRef;
-
-    @Column(name = "name", nullable = false)
     private String name;
-
-    @Column(name = "location", nullable = false)
     private String location;
-
-    @Column(name = "country", nullable = false)
     private String country;
-
-    @Column(name = "lat", nullable = false, columnDefinition = "numeric")
     private Double lat;
-
-    @Column(name = "lng", nullable = false, columnDefinition = "numeric")
     private Double lng;
-
-    @Column(name = "alt", nullable = false)
     private String alt;
-
-    @Column(name = "url", nullable = false)
     private String url;
 }
